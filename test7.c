d6c7d17653c83794c8927579f08aeab8bb089f3c
Ashutosh Kumar <102175051+ashutoshkr4458@users.noreply.github.com>
2025-04-22 00:01:21 +0530

    added test cases

---
+++ test7.c
@@ -0,0 +1,83 @@
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<pthread.h>
+#include<unistd.h>
+
+int axyz,b,c,d;
+pthread_mutex_t lck1, lck2;
+
+void write_(int* a, int val){
+	*a = val;
+}
+
+
+void global_vars(){
+        pthread_mutex_lock(&lck1);
+        pthread_mutex_unlock(&lck1);
+        write_(&axyz, 0);
+        write_(&b, 1);
+}
+
+void* txn1(void* arg){
+	sleep(1);
+	int r1 = axyz;
+	sleep(12);
+	int r2 = b;
+	axyz++;
+	b++;
+	return NULL;
+}
+
+void* txn2(void* arg){
+	sleep(3);
+	int r1 = c;
+	sleep(1);
+	int r2 = b;
+	sleep(2);
+	b++;
+	return NULL;
+}
+
+void* txn3(void* arg){
+	sleep(1);
+	int r1 = c;
+	sleep(2);
+	int r2 = axyz;
+	sleep(2);
+	c++;
+	return NULL;
+}
+
+
+void* thrd2(void* arg){
+	txn2(0);
+}
+	
+void* thrd1(void* arg){
+	txn1(0);
+	
+}
+
+void* thrd3(void* arg){
+	txn3(0);
+}
+
+int main(){
+	
+	printf("vars %p %p %p\n", &axyz, &b, &lck1); 
+	global_vars();
+	pthread_t t1,t2,t3;
+	//pthread_mutex_lock(&lck1);
+	//pthread_mutex_unlock(&lck1);
+	pthread_create(&t1, NULL, thrd1, NULL);
+	pthread_create(&t2, NULL, thrd2, NULL);
+	pthread_create(&t2, NULL, thrd3, NULL);
+	//txn2(0);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	printf("%d\n", axyz);
+
+    return 0;
+}
---
+++ test8.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<pthread.h>
+#include<unistd.h>
+
+int axyz,b,c,d;
+pthread_mutex_t lck1, lck2;
+
+void write_(int* a, int val){
+	*a = val;
+}
+
+
+void global_vars(){
+        pthread_mutex_lock(&lck1);
+        pthread_mutex_unlock(&lck1);
+        pthread_mutex_lock(&lck2);
+        pthread_mutex_unlock(&lck2);
+        write_(&axyz, 0);
+        write_(&b, 1);
+        write_(&c, 0);
+        write_(&d, 0);
+}
+
+void* txn1(void* arg){
+	
+	sleep(2);
+	axyz++;
+	sleep(2);
+	sleep(3);
+	c++;
+	return NULL;
+}
+
+void* txn2(void* arg){
+	b++;
+	sleep(3);
+	axyz++;
+	return NULL;
+}
+
+void* txn3(void* arg){
+	b++;
+	sleep(3);
+	c++;
+	
+	return NULL;
+}
+
+void* thrd2(void* arg){
+	txn2(0);
+}
+
+	
+void* thrd1(void* arg){
+	txn1(0);
+}
+
+void* thrd3(void* arg){
+	txn3(0);
+}
+
+
+int main(){
+	
+	printf("vars %p %p %p\n", &axyz, &b, &lck1); 
+	global_vars();
+	pthread_t t1,t2,t3;
+	//pthread_mutex_lock(&lck1);
+	//pthread_mutex_unlock(&lck1);
+	pthread_create(&t1, NULL, thrd1, NULL);
+	pthread_create(&t2, NULL, thrd2, NULL);
+	pthread_create(&t2, NULL, thrd3, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	printf("%d\n", axyz);
+
+    return 0;
+}
